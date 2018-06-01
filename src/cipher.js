773a6a42e9e8ee130a3711250dd0128a4f4ee279
Scarlett <scar.antoine@gmail.com>
2018-06-01 14:36:35 -0400

    la funcion decodificar si funciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,3 @@
-// const mensajeUsuario;
-// const offset;
-// const mensajeResuelto;
-// const ascii;
-// const mensajeEnNumeros;
-
 function encode() {
   const offset = parseInt(document.getElementById('recorrido').value);
   const mensajeUsuario = document.getElementById('string').value;
@@ -18,9 +12,16 @@ function encode() {
 
 }
 
-function decode(mensajeEncriptado, offset) {
-  const mensajeDesencriptado = '';
-  return mensajeDesencriptado;
+function decode() {
+  const offset = parseInt(document.getElementById('recorrido').value);
+  const mensajeUsuario = document.getElementById('string').value;
+  let mensajeEncriptado = '';
+  for(i=0; i<mensajeUsuario.length; i++) {
+    const codigoAscii = mensajeUsuario.charCodeAt(i);
+    const codigoAsciiNuevo = (codigoAscii - 65 - offset) % 26 + 65;
+    const deCodigoAcaracter = String.fromCharCode(codigoAsciiNuevo);
+    mensajeEncriptado = mensajeEncriptado + deCodigoAcaracter;  
+  }
+  document.getElementById('retorna-texto-cifrado').value = mensajeEncriptado;
+
 }
-
-
