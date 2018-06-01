b82d74301699a972425f08a408e3e6483a813406
Scarlett <scar.antoine@gmail.com>
2018-06-01 10:16:08 -0400

    la funcion para cifrar el mensaje ingresado por e usuario si fuenciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,26 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+// const mensajeUsuario;
+// const offset;
+// const mensajeResuelto;
+// const ascii;
+// const mensajeEnNumeros;
+
+function encode() {
+  const offset = parseInt(document.getElementById('recorrido').value);
+  const mensajeUsuario = document.getElementById('string').value;
+  let mensajeEncriptado = '';
+  for(i=0; i<mensajeUsuario.length; i++) {
+    const codigoAscii = mensajeUsuario.charCodeAt(i);
+    const codigoAsciiNuevo = (codigoAscii - 65 + offset) % 26 + 65;
+    const deCodigoAcaracter = String.fromCharCode(codigoAsciiNuevo);
+    mensajeEncriptado = mensajeEncriptado + deCodigoAcaracter;  
   }
+  document.getElementById('retorna-texto-cifrado').value = mensajeEncriptado;
+
 }
+
+function decode(mensajeEncriptado, offset) {
+  const mensajeDesencriptado = '';
+  return mensajeDesencriptado;
+}
+
+
